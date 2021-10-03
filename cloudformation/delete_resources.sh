4fe1bcdbd41571730a99d76da201a630710400e1
michal-hynek <hynek.michal@gmail.com>
2021-10-03 06:00:25 -0700

    Fixed delete_resources.sh

--- cloudformation/delete_resources.sh
+++ cloudformation/delete_resources.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-aws cloudformation delete-stack --stack-name PrimaryRDS --stack-region us-west-2 --profile udacity
-aws cloudformation delete-stack --stack-name SecondaryRDS --stack-region us-east-1 --profile udacity
-aws cloudformation delete-stack --stack-name PrimaryNetwork --stack-region us-west-2 --profile udacity
-aws cloudformation delete-stack --stack-name SecondaryNetwork --stack-region us-east-1 --profile udacity
+aws cloudformation delete-stack --stack-name PrimaryRDS --region us-west-2 --profile udacity
+aws cloudformation delete-stack --stack-name SecondaryRDS --region us-east-1 --profile udacity
+aws cloudformation delete-stack --stack-name PrimaryNetwork --region us-west-2 --profile udacity
+aws cloudformation delete-stack --stack-name SecondaryNetwork --region us-east-1 --profile udacity
+aws cloudformation delete-stack --stack-name PrimaryEC2 --region us-west-2 --profile udacity
+aws cloudformation delete-stack --stack-name SecondaryEC2 --region us-east-1 --profile udacity
