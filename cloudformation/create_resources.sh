c11a252f20ccbd090cc192c460a1d82535fdb633
michal-hynek <hynek.michal@gmail.com>
2021-10-02 17:52:51 -0700

    create_resources.sh updates

--- cloudformation/create_resources.sh
+++ cloudformation/create_resources.sh
@@ -14,7 +14,6 @@ aws cloudformation deploy \
 
 aws cloudformation deploy \
     --stack-name SecondaryNetwork \
-    --stack-region us-east-1 \
     --template-file ./vpc.yaml \
     --parameter-overrides VpcName=Secondary VpcCIDR=10.2.0.0/16 PublicSubnet1CIDR=10.2.10.0/24 PublicSubnet2CIDR=10.2.11.0/24 PrivateSubnet1CIDR=10.2.20.0/24 PrivateSubnet2CIDR=10.2.21.0/24 \
     --profile udacity \
@@ -37,16 +36,17 @@ SECONDARY_PRIVATE_SUBNET2_ID=`aws cloudformation describe-stacks --stack-name Se
 aws cloudformation deploy \
     --stack-name PrimaryRDS \
     --template-file ./rds.yaml \
-    --parameter-overrides Subnet1=$PRIMARY_PRIVATE_SUBNET1_ID Subnet2=$PRIMARY_PRIVATE_SUBNET2_ID DatabaseSecurityGroup=$PRIMARY_DB_SG DBUsername=root DBPassword=testtest1 SourceDBId='' SourceDBRegion='' \
+    --parameter-overrides DatabaseId=primary Subnet1=$PRIMARY_PRIVATE_SUBNET1_ID Subnet2=$PRIMARY_PRIVATE_SUBNET2_ID DatabaseSecurityGroup=$PRIMARY_DB_SG DBUsername=root DBPassword=testtest1 SourceDBId='' SourceDBRegion='' \
     --profile udacity \
     --region us-west-2
 
-PRIMARY_DB_ID=`aws cloudformation describe-stacks --stack-name PrimaryRDS --query "Stacks[0].Outputs[?OutputKey=='DatbaseId'].OutputValue" --output text --profile udacity --region us-west-2`
+PRIMARY_DB_ID=`aws cloudformation describe-stacks --stack-name PrimaryRDS --query "Stacks[0].Outputs[?OutputKey=='DatabaseId'].OutputValue" --output text --profile udacity --region us-west-2`
+PRIMARY_DB_ARN=`aws rds describe-db-instances --db-instance-identifier $PRIMARY_DB_ID --query "DBInstances[0].DBInstanceArn" --profile udacity --region us-west-2 --output text`
 
 aws cloudformation deploy \
     --stack-name SecondaryRDS \
     --template-file ./rds.yaml \
-    --parameter-overrides Subnet1=$SECONDARY_PRIVATE_SUBNET1_ID Subnet2=$SECONDARY_PRIVATE_SUBNET2_ID DatabaseSecurityGroup=$SECONDARY_DB_SG \
+    --parameter-overrides DatabaseId=secondary Subnet1=$SECONDARY_PRIVATE_SUBNET1_ID Subnet2=$SECONDARY_PRIVATE_SUBNET2_ID DatabaseSecurityGroup=$SECONDARY_DB_SG DBUsername=root DBPassword=testtest1 SourceDBId=$PRIMARY_DB_ARN SourceDBRegion=us-west-2 \
     --profile udacity \
     --region us-east-1
 
@@ -63,6 +63,6 @@ SECONDARY_IMAGE_ID=ami-09e67e426f25ce0d7 # ubuntu 20.04 in us-east-1
 aws cloudformation deploy \
     --stack-name SecondaryEC2 \
     --template-file ./ec2.yaml \
-    --parameter-overrides ApplicationSecurityGroup=$SECONDARY_APP_SG SSHKeyName=udacity-ssh InstanceImageId=$SECONDARY_IMAGE_ID SubnetId=$SECONDARY_PUBLIC_SUBNET1_ID \
+    --parameter-overrides ApplicationSecurityGroup=$SECONDARY_APP_SG SSHKeyName=udacity2-ssh InstanceImageId=$SECONDARY_IMAGE_ID SubnetId=$SECONDARY_PUBLIC_SUBNET1_ID \
     --profile udacity \
     --region us-east-1
